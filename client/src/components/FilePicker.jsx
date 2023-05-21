f9ac46cd228e329d86805438098cf6372d61a88f
Sandy Congreve <sandy.congreve@madepeople.se>
2023-05-21 14:40:59 +0200

    Modified filpicker and added corrections on type errors

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -8,26 +8,26 @@ const FilePicker = ({ file, setFile, readFile }) => {
         <input
           id="file-upload"
           type="file"
-          accept="image/"
+          accept="image/*"
           onChange={(e) => setFile(e.target.files[0])}
         />
-        <label htmlFor="file-upload" className="file-picker-label">
+        <label htmlFor="file-upload" className="filepicker-label">
           Upload File
         </label>
         <p className="mt-2 text-gray-500 text-xs truncate">
-          {file === "" ? "No file selected" : file.name}
+          {file === '' ? "No file selected" : file.name}
         </p>
         <div className="mt-4 flex flex-wrap gap-3">
           <CustomButton
             type="outline"
             title="Logo"
-            handleClick={() => readFile("logo")}
+            handleClick={() => readFile('logo')}
             customStyles="text-xs"
           />
           <CustomButton
             type="filled"
             title="Full"
-            handleClick={() => readFile("full")}
+            handleClick={() => readFile('full')}
             customStyles="text-xs"
           />
         </div>
