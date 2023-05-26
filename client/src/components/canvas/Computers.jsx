72344c85255b17f1301e50d5e68bd8f9b9eaf866
Sandy Congreve <sandy.congreve@madepeople.se>
2023-05-26 13:26:37 +0200

    Added hero section

--- client/src/components/canvas/Computers.jsx
+++ client/src/components/canvas/Computers.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
 
-const Computers = () => {
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  // GLTF
+  // use element mesh to create 3d based on the model
+  // hempisphere light to create light
+  //Adding pointlight to the scene
+  const computer = useGLTF("/desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />{" "}
+      <pointLight intensity={1} />{" "}
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    setIsMobile(mediaQuery.matches);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+  // frame loop demand
+  // shadows
+  // camera position important
+  // gl preserveDrawingBuffer needs to be true in order for the object to rendered properly
+  //Suspense allows you to wait for some code to load and render
+  //Orbits-controls - allows us to move around the scene from left to right and up and down and zoom in and out
+  // maxPolarAngle - allows us to move around the scene with some restrictions
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
