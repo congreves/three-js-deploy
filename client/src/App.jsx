23ef3789178520956e611b5f987c474edb40e7ab
Sandy Congreve <sandy.congreve@madepeople.se>
2023-05-26 09:42:36 +0200

    Added components and tailwind classes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,42 @@
-import Canvas from "./canvas"
-import Customizer from "./pages/Customizer"
-import Home from "./pages/Home"
-
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Tech,
+  Works,
+  StarsCanvas,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Experience,
+} from "./components";
 
 function App() {
-
   return (
-  <main className="app transition-all ease-in">
-    <Home />
-    <Canvas />
-    <Customizer />
-  </main>
-  )
+    <BrowserRouter>
+      <main className="app transition-all ease-in relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        {/* <Home />
+        <Canvas />
+        <Customizer /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </main>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,24 @@
-import CustomButton from "./CustomButton";
-import FilePicker from "./FilePicker";
-import ColorPicker from "./ColorPicker";
-import Tab from "./Tab";
-import AIPicker from "./AIPicker";
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Tech from './Tech';
+import Experience from './Experience';
+import Works from './Works';
+import Feedbacks from './Feedbacks';
+import Contact from './Contact';
 
-export { CustomButton, FilePicker, ColorPicker, Tab, AIPicker };
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
